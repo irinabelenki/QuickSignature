5b0c6bb704dea024f22c462c96e7d758bd009f12
Irina Belenki <irina.belenki@gmail.com>
2016-12-11 23:11:01 +0200

    Added TouchImageView and test button

--- app/src/main/java/com/irinabelenki/quicksignature/MainActivity.java
+++ app/src/main/java/com/irinabelenki/quicksignature/MainActivity.java
@@ -1,13 +1,141 @@
 package com.irinabelenki.quicksignature;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openImageButton;
+    private TouchImageView imageView;
+    private final int PICK_FILE_RESULT_CODE = 1000;
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openImageButton = (Button)findViewById(R.id.open_image_button);
+        openImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), PICK_FILE_RESULT_CODE);
+            }
+        });
+
+        openImageButton = (Button)findViewById(R.id.test_button);
+        openImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });
+
+        imageView = (TouchImageView)findViewById(R.id.image_view);
+        imageView.setMaxZoom(4f);
+        Log.i(TAG, "MainActivity onCreate");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PICK_FILE_RESULT_CODE:
+                    try {
+                        Uri selectedImageUri = data.getData();
+                        Toast.makeText(this, "URI from intent: " + selectedImageUri, Toast.LENGTH_LONG).show();
+                        Log.i(TAG, "URI from intent: " + selectedImageUri);
+
+                        Bitmap bitmap = getBitmapFromUri(selectedImageUri);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                    catch (Exception e) {
+                        Log.e(TAG, "Exception in onActivityResult: " + e.getMessage());
+                        Toast.makeText(this, "Exception in onActivityResult: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                    break;
+            }
+        } else {
+            Log.i(TAG, "onActivityResult: Result is not OK");
+            Toast.makeText(this, "onActivityResult: Result is not OK", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    private Bitmap createTopBitmap() {
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap tempBitmap = Bitmap.createBitmap(100, 100, conf); // this creates a MUTABLE bitmap
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(tempBitmap, 0, 0, null);
+        tempCanvas.drawRoundRect(new RectF(100, 100, 200, 200), 2, 2, new Paint());
+        return tempBitmap;
+    }
+
+    private Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
+        Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(), bmp1.getConfig());
+        Canvas canvas = new Canvas(bmOverlay);
+        canvas.drawBitmap(bmp1, new Matrix(), null);
+        canvas.drawBitmap(bmp2, new Matrix(), null);
+        return bmOverlay;
+    }
+
+    private void test() {
+        Bitmap bitmap = overlay(((BitmapDrawable)imageView.getDrawable()).getBitmap(),
+                createTopBitmap());
+        imageView.setImageBitmap(bitmap);
     }
 }
